9da0049cb0f08977c08df957f01ef11027819f2b
Aman Gaur <aman.gaur5505@gmail.com>
2022-05-09 07:59:09 +0530

    Added a  pug file

---
+++ pug/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{                                                                //This is the nedpoint we have given 
+    const con = "This is the best content on the internet so far so use it wisely"        // Contents of the endpoints
+    const params = {'title': 'PubG is the best game', "content": con}                     //Remember this format to execute the contents of endpoints
+    res.status(200).render('index.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
